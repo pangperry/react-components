d2d9476732644b4ad4edcb76683cb1cf59bb1a68
Mark Perry <mark.j.perry11@gmail.com>
2017-12-13 11:37:40 -0800

    refactor to dynamically call component

--- app.js
+++ app.js
@@ -5,6 +5,28 @@
 //       <li key={i}>{item}</li>)}
 //   </ul>
 // );
+// class GroceryListItems extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <ul>
+//         {this.props.items.map((item, i) =>
+//           <li key={i}>{item}</li>)}
+//       </ul>
+//     )
+//   }
+// }
+// ReactDOM.render(<GroceryListItems items={['apple', 'broccoli', 'steak']} />, document.getElementById("app"));
+
+
+var GroceryItem = (props) => (
+  <li>{props.item}</li>
+);
+
+
 class GroceryListItems extends React.Component {
   constructor(props) {
     super(props);
@@ -14,10 +36,10 @@ class GroceryListItems extends React.Component {
     return (
       <ul>
         {this.props.items.map((item, i) =>
-          <li key={i}>{item}</li>)}
+          <GroceryItem item={item} />
+          )}
       </ul>
     )
   }
 }
-
 ReactDOM.render(<GroceryListItems items={['apple', 'broccoli', 'steak']} />, document.getElementById("app"));
