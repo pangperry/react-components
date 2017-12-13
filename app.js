ae12d2e4f7b60a5e5de763dfc5c12ad0a784a390
Mark Perry <mark.j.perry11@gmail.com>
2017-12-13 11:09:35 -0800

    Add dynamic rendering, functional and class

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // TODO
+// var GroceryListItems = (props) => (
+//   <ul>
+//     {props.items.map((item,i) =>
+//       <li key={i}>{item}</li>)}
+//   </ul>
+// );
+class GroceryListItems extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.items.map((item, i) =>
+          <li key={i}>{item}</li>)}
+      </ul>
+    )
+  }
+}
+
+ReactDOM.render(<GroceryListItems items={['apple', 'broccoli', 'steak']} />, document.getElementById("app"));
